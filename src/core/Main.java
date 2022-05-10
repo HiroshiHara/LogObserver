a89ab209a3447e66ffb4e3bf6391a18e6a7136e4
HiroshiHara <mrbob403@gmail.com>
2022-05-10 23:52:37 +0900

    コンソールに差分を出力する際に余計な改行コードを出力しないように実装

--- src/core/Main.java
+++ src/core/Main.java
@@ -11,33 +11,43 @@ import thread.ObserveThread;
 /**
  * コマンドラインの第一引数に指定されたファイルの監視を開始する<br>
  * 指定するファイルは絶対パスで指定するか、<br>
- * カレントディレクトリにあるファイル名で指定する
+ * カレントディレクトリにあるファイル名で指定する<br>
+ * [実行コマンド] java -jar .\LogObserver.jar
+ * [第一引数(必須)] 監視対象ファイルの絶対パス OR カレントディレクトリにある監視対象ファイル名 
+ * [第二引数(任意)] 差分比較処理を何秒毎に行うか 整数で指定 未指定の場合1秒
  * @author mrbob
  *
  */
 public class Main {
+	public static Notificator g_diffNotificator;
+	
 	public static void main(String[] args) {
-		if (args.length != 1) {
+		if (args[0] == null) {
 			throw new IllegalArgumentException("引数にファイルの絶対パスか、カレントディレクトリにある対象のファイル名を渡してください。");
 		}
 		if (!args[0].endsWith(".txt") && !args[0].endsWith(".log")) {
 			throw new IllegalArgumentException("引数に指定するファイルの拡張子は.txtか.logとしてください。");
 		}
+		// TODO ApacheCommonsを使用して第二引数のバリデーションを実装
+	
+		
 		String p_currentDir = System.getProperty("user.dir");
 		File p_target1 = new File(args[0]);
 		File p_target2 = new File(p_currentDir + "\\" + args[0]);
 		
 		Thread p_thread = null;
+		Observer g_LogObserver = new LogObserver();
+		
+		// 指定ファイルが絶対パスに存在する場合
 		if (p_target1.exists()) {
-			Notificator p_diffNotificator = new DiffNotificator(p_target1);
-			Observer g_LogObserver = new LogObserver();
-			p_diffNotificator.addObserver(g_LogObserver);
-			p_thread = new ObserveThread(p_target1, p_diffNotificator);
+			g_diffNotificator = new DiffNotificator(p_target1);
+			g_diffNotificator.addObserver(g_LogObserver);
+			p_thread = new ObserveThread(p_target1, g_diffNotificator);
+		// 指定ファイルがカレントディレクトリに存在する場合
 		} else if(p_target2.exists()) {
-			Notificator p_diffNotificator = new DiffNotificator(p_target2);
-			Observer g_LogObserver = new LogObserver();
-			p_diffNotificator.addObserver(g_LogObserver);
-			p_thread = new ObserveThread(p_target2, p_diffNotificator);
+			g_diffNotificator = new DiffNotificator(p_target2);
+			g_diffNotificator.addObserver(g_LogObserver);
+			p_thread = new ObserveThread(p_target2, g_diffNotificator);
 		} else {
 			throw new IllegalArgumentException("引数に指定されたファイルが存在しません。");
 		}
--- src/notificator/DiffNotificator.java
+++ src/notificator/DiffNotificator.java
@@ -34,11 +34,79 @@ public class DiffNotificator extends Notificator {
 	private String g_diff = "";
 	
 	/**
+	 * 改行コード(CRLF)
+	 */
+	private final String CRLF = "\r\n";
+	
+	/**
+	 * 改行コード(LF)
+	 */
+	private final String LF = "\n";
+	
+	/**
+	 * 改行コード(CR)
+	 */
+	private final String CR = "\r";
+	
+	
+	/**
 	 * コンストラクタ
 	 * @param x_targetFile 監視対象のファイル
 	 */
 	public DiffNotificator(File x_targetFile) {
 		this.g_targetFile = x_targetFile;
+	}
+	
+	public void execDiff(String x_newText, String x_oldText) {
+		// 初回実行時、差分比較先テキストにセットして終了
+		if (getOldText().isBlank()) {
+			setOldText(x_newText);
+			return;
+		}
+		// 差分がない場合、差分テキストに空文字をセットして終了
+		if (getNewText().equals(getOldText())) {
+			setDiff("");
+			return;
+		// 差分比較処理(ログが対象なので新規行のみ求める)
+		} else {
+			// 比較元文字列の終了位置から最新文字列の終端までを切り出し
+			String p_diff = getNewText().substring(getOldText().length(), getNewText().length());
+			// 差分の開始が改行コード付きの場合取り除く
+			if (p_diff.startsWith(CRLF)) {
+				p_diff = p_diff.substring(CRLF.length());
+			} else if (p_diff.startsWith(LF)) {
+				p_diff = p_diff.substring(LF.length());
+			} else if (p_diff.startsWith(CR)) {
+				p_diff = p_diff.substring(CR.length());
+			}
+			setDiff(p_diff);
+			// 差分比較終了後、差分比較元を比較先に移動
+			setOldText(getNewText());
+		}
+		
+	}
+	
+	@Override
+	public void execute() {
+		Path p_file = Paths.get(getTargetFile().getAbsolutePath());
+		try {
+			String p_text = Files.readString(p_file);
+			if (!p_text.isEmpty()) {
+				setNewText(p_text);
+				execDiff(getNewText(), getOldText());
+				notifyObservers();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * 監視対象のファイルを取得する
+	 * @return 監視対象のファイル
+	 */
+	public File getTargetFile() {
+		return this.g_targetFile;
 	}
 	
 	/**
@@ -58,39 +126,36 @@ public class DiffNotificator extends Notificator {
 		this.g_diff = x_diff;
 	}
 	
-	public void execDiff(String x_newText, String x_oldText) {
-		// 初回実行時、差分比較先テキストにセットして終了
-		if (this.g_oldText.isBlank()) {
-			this.g_oldText = x_newText;
-			return;
-		}
-		// 差分がない場合、差分テキストに空文字をセットして終了
-		if (this.g_newText.equals(this.g_oldText)) {
-			setDiff("");
-			return;
-		// 差分比較処理(ログが対象なので新規行のみ求める)
-		} else {
-			// 比較元文字列の終了位置から最新文字列の終端までを切り出し
-			setDiff(g_newText.substring(g_oldText.length()+1, g_newText.length()));
-			// 差分比較終了後、差分比較元を比較先に移動
-			this.g_oldText = this.g_newText;
-		}
-		
+	/**
+	 * 差分比較元テキスト(常にファイル最新の内容)を取得する
+	 * @return 差分比較元テキスト(常にファイル最新の内容)
+	 */
+	public String getNewText() {
+		return this.g_newText;
 	}
 	
-	@Override
-	public void execute() {
-		Path p_file = Paths.get(this.g_targetFile.getAbsolutePath());
-		try {
-			String p_text = Files.readString(p_file);
-			if (!p_text.isEmpty()) {
-				this.g_newText = p_text;
-				execDiff(this.g_newText, this.g_oldText);
-				notifyObservers();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+	/**
+	 * 差分比較先テキスト(前回のファイルの内容)をセットする
+	 * @param x_newText 差分比較先テキスト(前回のファイルの内容)
+	 */
+	public void setNewText(String x_newText) {
+		this.g_newText = x_newText;
+	}
+	
+	/**
+	 * 差分比較先テキスト(前回のファイルの内容)を取得する
+	 * @return 差分比較先テキスト(前回のファイルの内容)
+	 */
+	public String getOldText() {
+		return this.g_oldText;
+	}
+	
+	/**
+	 * 差分比較元テキスト(常にファイル最新の内容)をセットする
+	 * @param x_newText 差分比較元テキスト(常にファイル最新の内容)
+	 */
+	public void setOldText(String x_oldText) {
+		this.g_oldText = x_oldText;
 	}
 	
 }
